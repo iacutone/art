cad01a10b2d4143471097de68b10260fb7719d5a
Eric Iacutone <eric.iacutone@gmail.com>
2014-04-27 22:54:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# A Repo for Helpful Art Tools
+
+# Todo
+
+grid.rb will be used to create a grid overlay over an image.
---
+++ grid.rb
@@ -0,0 +1,5 @@
+require 'image_size'
+require 'mini_magick'
+
+size = []
+size = ImageSize.path('watercolorgirl.jpg').size
